2c8666697f7361a663ac20677ec9d9f6e2b39721
Aayush <aayush.singh@hbwsl.com>
2025-05-07 09:25:19 +0530

    register book post type upon plugin activation

--- wp-book/wp-book.php
+++ wp-book/wp-book.php
@@ -10,9 +10,45 @@ defined('ABSPATH') || exit;
 
 register_activation_hook(__FILE__, 'wp_book_activate');
 function wp_book_activate() {
-    
+    wp_book_post_type_register();
     flush_rewrite_rules();
 }
+
+add_action('init', 'wp_book_post_type_register');
+
+function wp_book_post_type_register(){
+    $labels = array(
+        'name'               => 'Books',
+        'singular_name'      => 'Book',
+        'menu_name'          => 'Books',
+        'name_admin_bar'     => 'Book',
+        'add_new'            => 'Add New',
+        'add_new_item'       => 'Add New Book',
+        'new_item'           => 'New Book',
+        'edit_item'          => 'Edit Book',
+        'view_item'          => 'View Book',
+        'all_items'          => 'All Books',
+        'search_items'       => 'Search Books',
+        'not_found'          => 'No books found.',
+        'not_found_in_trash' => 'No books found in Trash.',
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'show_in_rest'       => true,
+        'has_archive'        => true,
+        'rewrite'            => array('slug' => 'books'),
+        'supports'           => array('title', 'editor', 'thumbnail', 'custom-fields'),
+        'menu_icon'          => 'dashicons-book',
+    );
+
+    register_post_type('book', $args);
+}
+
+
+
+
 
 
 
