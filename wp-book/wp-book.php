de78ba7f6d542d5a723239f0e38529e783f6e653
Aayush <aayush.singh@hbwsl.com>
2025-05-07 15:54:46 +0530

    add admin dashboard widget

--- wp-book/wp-book.php
+++ wp-book/wp-book.php
@@ -15,6 +15,7 @@ require_once plugin_dir_path(__FILE__) . 'includes/meta-box.php';
 require_once plugin_dir_path(__FILE__) . 'includes/meta-table.php';
 require_once plugin_dir_path(__FILE__) . 'includes/admin-settings.php';
 require_once plugin_dir_path(__FILE__) . 'includes/book-shortcode.php';
+require_once plugin_dir_path(__FILE__) . 'includes/block-book-sidebar.php';
 
 
 register_activation_hook(__FILE__, 'wp_book_activate');
@@ -24,4 +25,48 @@ function wp_book_activate() {
     wp_book_non_hierarchical_taxonomy();
     flush_rewrite_rules();
 }
+
+
+add_action('wp_dashboard_setup', 'wpb_register_dashboard_widget');
+
+function wpb_register_dashboard_widget() {
+    wp_add_dashboard_widget(
+        'wpb_top_categories_widget', 
+        'Top 5 Book Categories',     
+        'wpb_display_top_categories'
+    );
+}
+
+function wpb_display_top_categories() {
+    $terms = get_terms([
+        'taxonomy'   => 'book_category',  
+        'orderby'    => 'count',
+        'order'      => 'DESC',
+        'number'     => 5,
+        'hide_empty' => true,
+    ]);
+
+    if (empty($terms) || is_wp_error($terms)) {
+        echo '<p>No book categories found.</p>';
+        return;
+    }
+
+    echo '<ul>';
+    foreach ($terms as $term) {
+        echo '<li><strong>' . esc_html($term->name) . '</strong> — ' . esc_html($term->count) . ' books</li>';
+    }
+    echo '</ul>';
+}
+
+
+function loadMyBlock() {
+    wp_enqueue_script(
+      'my-new-block',
+      plugin_dir_url(__FILE__) . 'test-block.js',
+      array('wp-blocks','wp-editor'),
+      true
+    );
+  }
+     
+  add_action('enqueue_block_editor_assets', 'loadMyBlock');
 
